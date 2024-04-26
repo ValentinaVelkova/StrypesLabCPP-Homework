f490336ae369b09d0aa6b4a8c52df874365b264f
ValentinaVelkova <166612677+ValentinaVelkova@users.noreply.github.com>
2024-04-26 11:43:34 +0300

    Add files via upload

---
+++ Homework#1/Exercise4/maxnumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    float num1,num2, num3;
+    std::cin >> num1 >> num2 >> num3;
+    float max;
+
+    if (num1 > num2 && num1 > num3) {
+        max = num1;
+    } else if (num2 > num1 && num2 > num3) {
+        max = num2;
+    } else {
+        max = num3;
+    }
+
+    std::cout << "The max number is: " << max << std::endl;
+    
+    return 0;
+}
+
+
+   
+    
